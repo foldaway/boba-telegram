0385687c36680ab8ac356acf7642527eca2f94a5
Daniel Lee <explosivecow@me.com>
2019-01-16 18:36:29 +0800

    use scenes and add option between 3 stores

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const Telegraf = require('telegraf')
 
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
+const session = require('telegraf/session')
+const Stage = require('telegraf/stage')
+const Scene = require('telegraf/scenes/base')
+const { leave } = Stage
 
 var request = require("request")
 
@@ -29,8 +33,11 @@ request({
 var nearestLiho = {title:"", address:"", phone:"", latitude: 0, longitude:0, distance:0};
 var nearestKoi = {title:"", address:"", phone:"", latitude: 0, longitude:0, distance:0};
 var nearestGongCha = {title:"", address:"", phone:"", latitude: 0, longitude:0, distance:0};
+var myLocation;
 
-bot.start((ctx)=>{
+//start scene
+const sendLocation = new Scene('sendLocation');
+sendLocation.enter((ctx)=>{
 	return ctx.reply('Send your location.', Extra.markup((markup) => {
     return markup.resize()
       .keyboard([
@@ -38,7 +45,154 @@ bot.start((ctx)=>{
       ])
   	}))
 })
+sendLocation.on('location', (ctx)=>{
+	myLocation = ctx.message.location;
+	return ctx.reply('Select store.', Extra.markup((markup) => {
+    return markup.resize()
+      .keyboard([
+      	['Koi'],
+      	['LiHo'],
+      	['Gong Cha']
+      ])
+  	}))
+});
 
+//koi scene
+
+const koiScene = new Scene('koiScene');
+koiScene.enter((ctx)=>{
+	if(data != 0){
+		for (var i = 0; i < data.length; i++){
+			if(data[i].location == null){
+				i++;
+			}
+			var result = geoLib.getDistance(
+				{latitude: myLocation.latitude, longitude: myLocation.longitude},
+				{latitude: data[i].location.LATITUDE, longitude: data[i].location.LONGITUDE}
+			);
+			if (data[i].chain == "Koi"){
+				if ((nearestKoi.distance == 0) || (nearestKoi.distance > result)){
+					nearestKoi.title = data[i].title;
+					nearestKoi.address = data[i].address;
+					nearestKoi.phone = data[i].phone;
+					nearestKoi.latitude = data[i].location.LATITUDE;
+					nearestKoi.longitude = data[i].location.LONGITUDE;
+					nearestKoi.distance = result;
+				}
+			}
+		}
+		ctx.reply(`Nearest Koi\n${nearestKoi.title}\n${nearestKoi.address}\n${nearestKoi.phone}\n${nearestKoi.distance}m`);
+		ctx.replyWithLocation(nearestKoi.latitude, nearestKoi.longitude);
+		nearestKoi =  {title:"", address:"", phone:"", latitude: 0, longitude:0, distance:0};
+		ctx.scene.leave();
+		ctx.scene.enter('sendLocation');
+	} 
+	else{
+		ctx.reply(`No data`);
+	}
+})
+
+//LiHo Scene
+const LiHoScene = new Scene('LiHoScene');
+LiHoScene.enter((ctx)=>{
+	if(data != 0){
+		for (var i = 0; i < data.length; i++){
+			if(data[i].location == null){
+				i++;
+			}
+			var result = geoLib.getDistance(
+				{latitude: myLocation.latitude, longitude: myLocation.longitude},
+				{latitude: data[i].location.LATITUDE, longitude: data[i].location.LONGITUDE}
+			);
+			if (data[i].chain == "LiHO"){
+				if ((nearestLiho.distance == 0) || (nearestLiho.distance > result)){
+					nearestLiho.title = data[i].title;
+					nearestLiho.address = data[i].address;
+					nearestLiho.phone = data[i].phone;
+					nearestLiho.latitude = data[i].location.LATITUDE;
+					nearestLiho.longitude = data[i].location.LONGITUDE;
+					nearestLiho.distance = result;
+				}
+			}
+		}
+		ctx.reply(`Nearest LiHo\n${nearestLiho.title}\n${nearestLiho.address}\n${nearestLiho.phone}\n${nearestLiho.distance}m`);
+		ctx.replyWithLocation(nearestLiho.latitude, nearestLiho.longitude);
+		nearestLiho =  {title:"", address:"", phone:"", latitude: 0, longitude:0, distance:0};
+		ctx.scene.leave();
+		ctx.scene.enter('sendLocation');
+	} 
+	else{
+		ctx.reply(`No data`);
+	}
+})
+
+
+//Gong Cha Scene
+
+const GongChaScene = new Scene('GongChaScene');
+GongChaScene.enter((ctx)=>{
+	if(data != 0){
+		for (var i = 0; i < data.length; i++){
+			if(data[i].location == null){
+				i++;
+			}
+			var result = geoLib.getDistance(
+				{latitude: myLocation.latitude, longitude: myLocation.longitude},
+				{latitude: data[i].location.LATITUDE, longitude: data[i].location.LONGITUDE}
+			);
+			if (data[i].chain == "Gong Cha"){
+				if ((nearestGongCha.distance == 0) || (nearestGongCha.distance > result)){
+					nearestGongCha.title = data[i].title;
+					nearestGongCha.address = data[i].address;
+					//nearestGongCha.phone = data[i].phone;
+					nearestGongCha.latitude = data[i].location.LATITUDE;
+					nearestGongCha.longitude = data[i].location.LONGITUDE;
+					nearestGongCha.distance = result;
+				}
+			}
+		}
+		ctx.reply(`Nearest Gong Cha\n${nearestGongCha.title}\n${nearestGongCha.address}\n${nearestGongCha.distance}m`);
+		ctx.replyWithLocation(nearestGongCha.latitude, nearestGongCha.longitude);
+		nearestGongCha =  {title:"", address:"", phone:"", latitude: 0, longitude:0, distance:0};
+		ctx.scene.leave();
+		ctx.scene.enter('sendLocation');
+	} 
+	else{
+		ctx.reply(`No data`);
+	}
+})
+
+
+sendLocation.hears('Koi', (ctx)=>{
+	ctx.scene.leave();
+	ctx.scene.enter('koiScene');
+})
+
+sendLocation.hears('LiHo', (ctx)=>{
+	ctx.scene.leave();
+	ctx.scene.enter('LiHoScene');
+})
+sendLocation.hears('Gong Cha', (ctx)=>{
+	ctx.scene.leave();
+	ctx.scene.enter('GongChaScene');
+})
+
+
+// Create scene manager
+const stage = new Stage()
+stage.command('cancel', leave())
+
+// Scene registration
+stage.register(sendLocation)
+stage.register(koiScene)
+stage.register(LiHoScene)
+stage.register(GongChaScene);
+bot.use(session())
+bot.use(stage.middleware())
+bot.start((ctx)=>{
+	ctx.scene.enter('sendLocation');
+})
+/*
 bot.on('location', (ctx) => {
 	if(data != 0){
 		for (var i = 0; i < data.length; i++){
@@ -85,14 +239,6 @@ bot.on('location', (ctx) => {
 		ctx.reply(`No data`);
 	}
 }); 
-
-bot.command('nearby', (ctx)=>{
-	return ctx.reply('Send your location.', Extra.markup((markup) => {
-    return markup.resize()
-      .keyboard([
-        markup.locationRequestButton('Send location')
-      ])
-  	}))
-})
+*/
 
 bot.startPolling()
