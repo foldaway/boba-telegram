7ba1879dec973e41c201ec53e1a52da119794caa
Duncan Leo <hello@duncanleo.me>
2019-10-21 00:29:17 +0800

    use single main scene

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
  */
 
 const url = 'https://bottleneckco.github.io/sg-scraper/boba.json';
-let data = 0;
+let data = null;
 request({
   url,
   json: true,
@@ -46,25 +46,15 @@ request({
     data = body;
   }
 });
-let nearestLiho = {
-  title: '', address: '', phone: '', latitude: 0, longitude: 0, distance: 0,
-};
-let nearestKoi = {
-  title: '', address: '', phone: '', latitude: 0, longitude: 0, distance: 0,
-};
-let nearestGongCha = {
-  title: '', address: '', phone: '', latitude: 0, longitude: 0, distance: 0,
-};
-let myLocation;
 
 // start scene
-const sendLocation = new Scene('sendLocation');
-sendLocation.enter(ctx => ctx.reply('Send your location.', Extra.markup(markup => markup.resize()
+const mainScene = new Scene('main');
+mainScene.enter(ctx => ctx.reply('Send your location.', Extra.markup(markup => markup.resize()
   .keyboard([
     markup.locationRequestButton('Send location'),
   ]))));
-sendLocation.on('location', (ctx) => {
-  myLocation = ctx.message.location;
+mainScene.on('location', (ctx) => {
+  ctx.scene.state.location = ctx.message.location;
   return ctx.reply('Select store.', Extra.markup(markup => markup.resize()
     .keyboard([
       ['Koi'],
@@ -72,144 +62,47 @@ sendLocation.on('location', (ctx) => {
       ['Gong Cha'],
     ])));
 });
-
-// koi scene
-
-const koiScene = new Scene('koiScene');
-koiScene.enter((ctx) => {
-  if (data != 0) {
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].location == null) {
-        i++;
-      }
-      const result = geoLib.getDistance(
-        { latitude: myLocation.latitude, longitude: myLocation.longitude },
-        { latitude: data[i].location.LATITUDE, longitude: data[i].location.LONGITUDE },
-      );
-      if (data[i].chain == 'Koi') {
-        if ((nearestKoi.distance == 0) || (nearestKoi.distance > result)) {
-          nearestKoi.title = data[i].title;
-          nearestKoi.address = data[i].address;
-          nearestKoi.phone = data[i].phone;
-          nearestKoi.latitude = data[i].location.LATITUDE;
-          nearestKoi.longitude = data[i].location.LONGITUDE;
-          nearestKoi.distance = result;
-        }
-      }
-    }
-    ctx.reply(`Nearest Koi\n${nearestKoi.title}\n${nearestKoi.address}\n${nearestKoi.phone}\n${nearestKoi.distance}m`);
-    ctx.replyWithLocation(nearestKoi.latitude, nearestKoi.longitude);
-    nearestKoi = {
-      title: '', address: '', phone: '', latitude: 0, longitude: 0, distance: 0,
-    };
-    ctx.reply('Enter /start to search for another store');
-    ctx.scene.leave();
-  } else {
+mainScene.on('message', (ctx) => {
+  if (data === null) {
     ctx.reply('No data');
-  }
-});
-
-// LiHo Scene
-const LiHoScene = new Scene('LiHoScene');
-LiHoScene.enter((ctx) => {
-  if (data != 0) {
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].location == null) {
-        i++;
-      }
-      const result = geoLib.getDistance(
-        { latitude: myLocation.latitude, longitude: myLocation.longitude },
-        { latitude: data[i].location.LATITUDE, longitude: data[i].location.LONGITUDE },
-      );
-      if (data[i].chain == 'LiHO') {
-        if ((nearestLiho.distance == 0) || (nearestLiho.distance > result)) {
-          nearestLiho.title = data[i].title;
-          nearestLiho.address = data[i].address;
-          nearestLiho.phone = data[i].phone;
-          nearestLiho.latitude = data[i].location.LATITUDE;
-          nearestLiho.longitude = data[i].location.LONGITUDE;
-          nearestLiho.distance = result;
-        }
-      }
-    }
-    ctx.reply(`Nearest LiHo\n${nearestLiho.title}\n${nearestLiho.address}\n${nearestLiho.phone}\n${nearestLiho.distance}m`);
-    ctx.replyWithLocation(nearestLiho.latitude, nearestLiho.longitude);
-    nearestLiho = {
-      title: '', address: '', phone: '', latitude: 0, longitude: 0, distance: 0,
-    };
-    ctx.reply('Enter /start to search for another store');
     ctx.scene.leave();
-  } else {
-    ctx.reply('No data');
+    return;
   }
-});
-
-
-// Gong Cha Scene
-
-const GongChaScene = new Scene('GongChaScene');
-GongChaScene.enter((ctx) => {
-  if (data != 0) {
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].location == null) {
-        i++;
-      }
-      const result = geoLib.getDistance(
-        { latitude: myLocation.latitude, longitude: myLocation.longitude },
-        { latitude: data[i].location.LATITUDE, longitude: data[i].location.LONGITUDE },
+  /** @type {Array<Outlet>} */
+  const nearestChains = data
+    .filter(outlet => outlet.chain === ctx.update.message.text)
+    .sort((a, b) => {
+      const aDist = geoLib.getDistance(
+        ctx.scene.state.location,
+        { latitude: a.location.LATITUDE, longitude: a.location.LONGITUDE },
       );
-      if (data[i].chain == 'Gong Cha') {
-        if ((nearestGongCha.distance == 0) || (nearestGongCha.distance > result)) {
-          nearestGongCha.title = data[i].title;
-          nearestGongCha.address = data[i].address;
-          // nearestGongCha.phone = data[i].phone;
-          nearestGongCha.latitude = data[i].location.LATITUDE;
-          nearestGongCha.longitude = data[i].location.LONGITUDE;
-          nearestGongCha.distance = result;
-        }
-      }
-    }
-    ctx.reply(`Nearest Gong Cha\n${nearestGongCha.title}\n${nearestGongCha.address}\n${nearestGongCha.distance}m`);
-    ctx.replyWithLocation(nearestGongCha.latitude, nearestGongCha.longitude);
-    nearestGongCha = {
-      title: '', address: '', phone: '', latitude: 0, longitude: 0, distance: 0,
-    };
-    ctx.reply('Enter /start to search for another store');
-    ctx.scene.leave();
-  } else {
+      const bDist = geoLib.getDistance(
+        ctx.scene.state.location,
+        { latitude: b.location.LATITUDE, longitude: b.location.LONGITUDE },
+      );
+      return aDist - bDist;
+    });
+  if (nearestChains.length === 0) {
     ctx.reply('No data');
+    ctx.scene.leave();
+    return;
   }
-});
-
-
-sendLocation.hears('Koi', (ctx) => {
+  ctx.reply(`Nearest ${ctx.update.message.text}\n${nearestChains[0].title}\n${nearestChains[0].address}\n${nearestChains[0].phone}\n${nearestChains[0].distance}m`);
+  ctx.replyWithLocation(nearestChains[0].location.LATITUDE, nearestChains[0].location.LONGITUDE);
+  ctx.reply('Enter /start to search for another store');
   ctx.scene.leave();
-  ctx.scene.enter('koiScene');
 });
-
-sendLocation.hears('LiHo', (ctx) => {
-  ctx.scene.leave();
-  ctx.scene.enter('LiHoScene');
-});
-sendLocation.hears('Gong Cha', (ctx) => {
-  ctx.scene.leave();
-  ctx.scene.enter('GongChaScene');
-});
-
 
 // Create scene manager
 const stage = new Stage();
 stage.command('cancel', leave());
+stage.register(mainScene);
 
 // Scene registration
-stage.register(sendLocation);
-stage.register(koiScene);
-stage.register(LiHoScene);
-stage.register(GongChaScene);
 bot.use(session());
 bot.use(stage.middleware());
 bot.start((ctx) => {
-  ctx.scene.enter('sendLocation');
+  ctx.scene.enter('main');
 });
 /*
 bot.on('location', (ctx) => {
